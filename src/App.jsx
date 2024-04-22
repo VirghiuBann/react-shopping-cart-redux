062305dc5ee57859d5a0d221d9255ce2c857d3f5
virghiu <virghiu@gmail.com>
2024-04-22 19:18:06 -0400

    create modal confirm

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CartContainer from './components/CartContainer'
 import NavBar from './components/NavBar'
 import { useEffect } from 'react'
 import { calculateTotals } from './features/cart/cartSlice'
+import Modal from './components/Modal'
 
 function App() {
   const { cartItems } = useSelector((state) => state.cart)
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <main>
+      <Modal />
       <NavBar />
       <CartContainer />
     </main>
