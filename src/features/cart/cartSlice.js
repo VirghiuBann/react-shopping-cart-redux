5300ae0fae45dab5ed1984f1edadb6b6ca78965f
virghiu <virghiu@gmail.com>
2024-04-25 18:43:36 -0400

    add axios, extrareducers builder

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,13 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+// import cartItems from '../../cartItems'
 
-import cartItems from '../../cartItems'
+const url = 'https://www.course-api.com/react-useReducer-cart-project'
 
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   totalQty: 4,
   totalAmount: 0,
   isLoading: true,
 }
+
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  async (_, thunkAPI) => {
+    try {
+      // console.log(name)
+      // console.log(thunkAPI)
+      const resp = await axios(url)
+
+      return resp.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong.')
+    }
+  }
+)
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
@@ -21,22 +39,36 @@ const cartSlice = createSlice({
     },
     increase: (state, { payload }) => {
       const cartItem = state.cartItems.find((item) => item.id === payload.id)
-      cartItem.quantity = cartItem.quantity + 1
+      cartItem.amount = cartItem.amount + 1
     },
     decrease: (state, { payload }) => {
       const cartItem = state.cartItems.find((item) => item.id === payload.id)
-      cartItem.quantity = cartItem.quantity - 1
+      cartItem.amount = cartItem.amount - 1
     },
     calculateTotals: (state) => {
       let totalQty = 0
       let totalAmount = 0
-      state.cartItems.forEach((item) => {
-        totalQty += item.quantity
-        totalAmount += item.quantity * item.price
+      state.cartItems.forEach(({ price, amount: quantity }) => {
+        totalQty += quantity
+        totalAmount += quantity * price
       })
       state.totalQty = totalQty
       state.totalAmount = parseFloat(totalAmount.toFixed(2))
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        console.log(action)
+        state.isLoading = false
+        state.cartItems = action.payload
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        state.isLoading = false
+      })
   },
 })
 
